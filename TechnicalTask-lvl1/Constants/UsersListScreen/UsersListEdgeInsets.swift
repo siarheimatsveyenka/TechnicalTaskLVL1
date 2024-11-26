280f9c05e1388cbb4287ec7b1b0ef13935a3877c
siarheimatsveyenka <siarhei.matsveyenka@innowise.com>
2024-11-27 00:17:35 +0300

    feat: Implement cell's spacing Implemented cell's spacing Implemented cell's rounded borders

--- TechnicalTask-lvl1/Constants/UsersListScreen/UsersListEdgeInsets.swift
+++ TechnicalTask-lvl1/Constants/UsersListScreen/UsersListEdgeInsets.swift
@@ -9,5 +9,5 @@ import UIKit
 
 enum UsersListEdgeInsets {
     static let usersList = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-    static let cellStackView = UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+    static let cellStackView = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
 }
